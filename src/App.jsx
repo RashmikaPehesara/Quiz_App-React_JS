3156fd216e1eac9fd68fe5fece86d9771f345222
RashmikaPehesara <rashmikapehesara2022@gmail.com>
2025-12-23 20:39:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./index.css";
+import Quiz from './Components/quiz';
+import Results from './Components/results';
+
+
+function App() {
+  return (
+    <div className="app-container"> 
+    <h1> Quiz App </h1>
+
+    <Quiz/>
+    
+
+
+    </div>
+  )
+}
+
+export default App
